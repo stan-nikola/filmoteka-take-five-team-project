a82c7e32a69aa68f3ccc98676e230c9176c76362
Maksym Shvets <maxim27061993@outlook.com>
2022-11-27 20:49:04 +0100

    LocalStorage v0.04

--- src/js/addLocalStorage.js
+++ src/js/addLocalStorage.js
@@ -1,42 +1,42 @@
-const objectFilmRef = {
-    picture: document.querySelector("picture"),
-    title: document.querySelector(".name-film"),
-    genre: document.querySelector(".genre-value-list"),
-    popularity: document.querySelector(".popularity-value-list")
-}
+// const objectFilmRef = {
+//     picture: document.querySelector("picture"),
+//     title: document.querySelector(".name-film"),
+//     genre: document.querySelector(".genre-value-list"),
+//     popularity: document.querySelector(".popularity-value-list")
+// }
 
-const buttonRef = {
-    addWatched: document.querySelector(".btn-watched"),
-    addQueue: document.querySelector(".btn-queue")
-}
+// const buttonRef = {
+//     addWatched: document.querySelector(".btn-watched"),
+//     addQueue: document.querySelector(".btn-queue")
+// }
 
-function addWatchedlocalStorage(film) {
-    if (localStorage.getItem("filmsWatched") === null) {
-        let films = { film };
-        localStorage.setItem("filmsWatched", JSON.stringify(films));
-    } else {
-        let films = Array.from(JSON.parse(localStorage.getItem("filmsWatched")));
-        films.push(film);
-        localStorage.setItem("filmsWatched", JSON.stringify(films));
-    }
-};
+// function addWatchedlocalStorage(film) {
+//     if (localStorage.getItem("filmsWatched") === null) {
+//         let films = { film };
+//         localStorage.setItem("filmsWatched", JSON.stringify(films));
+//     } else {
+//         let films = Array.from(JSON.parse(localStorage.getItem("filmsWatched")));
+//         films.push(film);
+//         localStorage.setItem("filmsWatched", JSON.stringify(films));
+//     }
+// };
 
-function addQueuelocalStorage(film) {
-    if (localStorage.getItem("filmsQueue") === null) {
-        let films = { film };
-        localStorage.setItem("filmsQueue", JSON.stringify(films));
-    } else {
-        let films = Array.from(JSON.parse(localStorage.getItem("filmsQueue")));
-        console.log(films);
-        films.push(film);
-        localStorage.setItem("filmsQueue", JSON.stringify(films));
-    }
-};
-//----------lISTENERS------------------------------------
+// function addQueuelocalStorage(film) {
+//     if (localStorage.getItem("filmsQueue") === null) {
+//         let films = { film };
+//         localStorage.setItem("filmsQueue", JSON.stringify(films));
+//     } else {
+//         let films = Array.from(JSON.parse(localStorage.getItem("filmsQueue")));
+//         console.log(films);
+//         films.push(film);
+//         localStorage.setItem("filmsQueue", JSON.stringify(films));
+//     }
+// };
+// //----------lISTENERS------------------------------------
 
-buttonRef.addWatched.addEventListener("click", () => {
-    addWatchedlocalStorage(objectFilmRef);
-})
-buttonRef.addQueue.addEventListener("click", () => {
-    addQueuelocalStorage(objectFilmRef);
-})
+// buttonRef.addWatched.addEventListener("click", () => {
+//     addWatchedlocalStorage(objectFilmRef);
+// })
+// buttonRef.addQueue.addEventListener("click", () => {
+//     addQueuelocalStorage(objectFilmRef);
+// })
--- src/js/getLocalStorage.js
+++ src/js/getLocalStorage.js
@@ -1,10 +1,10 @@
 // const Ref = {
-//     watchedBtn: document.querySelector(""),
-//     queueBtn: document.querySelector(""),
-//     library: document.querySelector(".library__gallery")
+//     watchedBtn: document.querySelector(".header__watched__btn__js"),
+//     queueBtn: document.querySelector(".header__queue__btn__js"),
+//     library: document.querySelector(".library__gallery__js")
 // }
 
-// watchedBtnRef.addEventListener("click", () => {
+// if (localStorage.getItem("filmsWatched") !== null) {
 //     let filmsLibrary = JSON.parse(localStorage.getItem("filmsWatched"));
 //     let markup = filmsLibrary.map((film) => {
 //         return `<li class=""card-container>
@@ -17,14 +17,30 @@
 //         </p>
 //         </li>`
 //     })
+//     Ref.library.insertAdjacentHTML("beforeend", markup.join(""));
 
-//     libraryRef.insertAdjacentHTML("beforeend", markup.join(""));
-// });
+//     Ref.watchedBtn.addEventListener("click", () => {
+//         Ref.library.textContent = "";
+//         let filmsLibrary = JSON.parse(localStorage.getItem("filmsWatched"));
+//         let markup = filmsLibrary.map((film) => {
+//             return `<li class=""card-container>
+//         <img class="image-poster" src="${film.src}" alt="">
+//         <p class="movie-data">
+//         ${film.title} <br>
+//         <span class="genre-year">
+//         ${film.genre}
+//         </span>
+//         </p>
+//         </li>`
+//         })
+//         Ref.library.insertAdjacentHTML("beforeend", markup.join(""));
+//     });
 
-// queueBtnRef.addEventListener("click", () => {
-// let filmsLibrary = JSON.parse(localStorage.getItem("filmsQueue"));
-// let markup = filmsLibrary.map((film) => {
-//     return `<li class=""card-container>
+//     Ref.queueBtn.addEventListener("click", () => {
+//         Ref.library.textContent = "";
+//         let filmsLibrary = JSON.parse(localStorage.getItem("filmsQueue"));
+//         let markup = filmsLibrary.map((film) => {
+//             return `<li class=""card-container>
 //   <img class="image-poster" src="${film.src}" alt="">
 //         <p class="movie-data">
 //         ${film.title} <br>
@@ -33,7 +49,11 @@
 //         </span>
 //         </p>
 //         </li>`
-// })
-
-// Ref.library.insertAdjacentHTML("beforeend", markup.join(""));
-// });
+//         })
+//         Ref.library.insertAdjacentHTML("beforeend", markup.join(""));
+//     });
+// } else {
+//     Ref.library.insertAdjacentHTML("beforeend", "Gallery is empy");
+//     Ref.library.classList.remove("movie-cards-gallery");
+//     Ref.library.setAttribute("style", `display:flex;margin:auto;justify-content: center`)
+// }
