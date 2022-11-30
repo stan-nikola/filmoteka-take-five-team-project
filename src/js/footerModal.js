7fd27daa3d57d2e60f40a9577698fbeea3e6cc81
Natalie Tkachenko <tkachenko_nataliya83@ukr.net>
2022-11-30 23:03:03 +0200

    bug-fix/footer/modal close
    Поправила стили, добавила функции закрітия

--- src/js/footerModal.js
+++ src/js/footerModal.js
@@ -1,29 +1,22 @@
-import team from '../data/team.json';
+import { team } from '../data/team.js';
+
+const footerRefs = {
+footerModalBtn: document.querySelector( '.js-modal' ),
+footerModalBackdrop: document.querySelector( ".footer__backdrop" ),
+teamList: document.querySelector( ".team__list" ),
+footerModalCloseBtn: document.querySelector( '.modal-close__btn' ),
+}
 
 
-const modalBtn = document.querySelector( '.js-modal' );
-console.log( modalBtn );
+footerRefs.footerModalBtn.addEventListener( "click", showModalHandler );
 
-const modalBackdrop = document.querySelector( ".footer__backdrop" );
-console.log( modalBackdrop );
-
-const teamList = document.querySelector( ".team__list" );
-console.log( teamList );
-
-console.log( team );
-
-
-
-modalBtn.addEventListener( "click", showModalHandler );
 
 function showModalHandler( event )
 {
-    console.log( modalBackdrop );
-    console.log( team );
-    modalBackdrop.classList.remove( "hidden" );
+   
+    footerRefs.footerModalBackdrop.classList.remove( "hidden" );
 
-    const phe = [ 1, 2, 3, 4 ]
-    
+        
     const teamMarkup = team.map( ( item ) => ` <li class="team__list-item">
 
                     <div class="teammate__card">
@@ -98,88 +91,34 @@ function showModalHandler( event )
 
             </li>`).join( "" );
     
-    teamList.innerHTML = teamMarkup;
+    footerRefs.teamList.innerHTML = teamMarkup;
 
-    
-    
+    footerRefs.footerModalCloseBtn.addEventListener( 'click', closeFooterModalHandler );
+    window.addEventListener( 'keydown', onKeyCloseFooterModal );
+    footerRefs.footerModalBackdrop.addEventListener( 'click', onBackdropClickClose );
+  
+}
 
-    
+function closeFooterModalHandler()
+{
+    footerRefs.footerModalBackdrop.classList.add('hidden');
+    window.removeEventListener('keydown', onKeyCloseFooterModal);
+    footerRefs.footerModalBackdrop.removeEventListener('click', onBackdropClickClose);
+ }
+
+
+
+function onKeyCloseFooterModal(evt) {
+  const ESC_KEY_CODE = 'Escape'
+  evt.preventDefault();
+  if (evt.code === ESC_KEY_CODE) {
+    closeFooterModalHandler()
+  }
 }
 
 
-
-
-            // <li class="team__list-item">
-
-            //         <div class="teammate__card">
-            //     <div class="img__thumb">
-            //         <img src="./images/team/default-img-team.jpeg" alt="Никто, абсолютно никто" class="teammate__picture">
-
-            //     </div>
-
-            //     <div class="teammate__info">
-            //         <div class="info__thumb">
-            //             <p class="name">Станислав Карманов</p>
-            //             <p class="role">Тим лид</p>
-            //         </div>
-            //         <p class="responsibilities">Архитектура проекта, определение единых практик разработки, сode review,
-            //             наставничество команды, распределение скоупа проекта между разработчиками</p>
-
-
-            //     </div>
-
-            //     <div class="social__nets">
-
-            //         <ul class="team__nets-list">
-
-            //             <li class="team__nets-list-item">
-            //                 <a href="" class="team__net-icon-link">
-            //                     <div class="team__net-icon-thumb">
-            //                         <svg class="team__net-icon" width="20px" height="20px">
-            //                             <use href="./images/icons/icons-opt.svg#icon-instagram-opt"></use>
-            //                         </svg>
-            //                     </div>
-            //                 </a>
-            //             </li>
-
-            //             <li class="team__nets-list-item">
-            //                 <a href="" class="team__net-icon-link">
-            //                     <div class="team__net-icon-thumb">
-            //                         <svg class="team__net-icon" width="20px" height="20px">
-            //                             <use href="./images/icons/icons.svg#icon-twitter"></use>
-            //                         </svg>
-            //                     </div>
-            //                 </a>
-            //             </li>
-
-            //             <li class="team__nets-list-item">
-            //                 <a href="" class="team__net-icon-link">
-            //                     <div class="team__net-icon-thumb">
-            //                         <svg class="team__net-icon" width="20px" height="20px">
-            //                             <use href="./images/icons/icons.svg#icon-facebook"></use>
-            //                         </svg>
-            //                     </div>
-            //                 </a>
-            //             </li>
-
-            //             <li class="team__nets-list-item">
-            //                 <a href="" class="team__net-icon-link">
-            //                     <div class="team__net-icon-thumb">
-            //                         <svg class="team__net-icon" width="20px" height="20px">
-            //                             <use href="./images/icons/icons.svg#icon-linkedin"></use>
-            //                         </svg>
-            //                     </div>
-            //                 </a>
-            //             </li>
-
-
-
-            //         </ul>
-
-
-            //     </div>
-
-
-            // </div>
-
-            // </li>
+function onBackdropClickClose (ev) {
+  if(ev.currentTarget === ev.target) {
+    closeFooterModalHandler();
+  }
+}
