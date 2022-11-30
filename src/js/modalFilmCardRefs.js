3143ea40bb10ccdd92431c71e540801f35f66cd0
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2022-11-30 15:28:18 +0200

    merge  modal fix bugs

--- src/js/modalFilmCardRefs.js
+++ src/js/modalFilmCardRefs.js
@@ -1,7 +1,9 @@
-export default refs = {
+const refs = {
   cardLi: document.querySelector('.card-container'),
   modalCard: document.querySelector('.modal-card__container-content'),
   movieContainer: document.querySelector('.movie-cards-gallery'),
   backdrop: document.querySelector('.backdrop'),
   btnClose: document.querySelector('.close-btn-js'),
 };
+
+export default refs;
