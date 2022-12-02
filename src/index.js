26fca95a9d7678a96562409b8e796bf31989ed7c
Maksym Shvets <maxim27061993@outlook.com>
2022-12-02 20:59:36 +0100

    NightOfSwift

--- src/index.js
+++ src/index.js
@@ -3,22 +3,21 @@ import * as footer from './js/footerModal';
 import searchByName from './js/searchByName';
 import onSubmit from './js/searchByName';
 import { onUpBtnClick } from './js/scrollPage';
-import * as theme from './js/colorTheme';
-
+import { addThemeLocalStorage, useDarkTheme, refsTheme } from './js/colorTheme';
 // Imports from modalFilmCard section-----
 import refs from './js/modalFilmCardRefs';
 import { renderModalFilmCard, closeModal } from './js/modalFilmCardRender';
-
 // ---------------------------------------
 import { renderMovies2 } from './js/pagination';
 import { renderMovies } from './js/renderHomeFilms';
 import { renderMovies2 } from './js/pagination';
-
+window.onload = useDarkTheme;
+addThemeLocalStorage();
 // Slider starter
 appendMovies();
 
 renderMovies();
-
+refsTheme.checkbox.addEventListener('click', useDarkTheme);
 // Modal listeners------------------------
 refs.movieContainer.addEventListener('click', renderModalFilmCard);
 refs.btnClose.addEventListener('click', closeModal);
--- src/indexLibrary.js
+++ src/indexLibrary.js
@@ -2,9 +2,13 @@
 import refs from './js/modalFilmCardRefs';
 import { renderModalFilmCard, closeModal } from './js/modalFilmCardRender';
 import { getLocalStorage } from './js/localStorage/getLocalStorage';
+import { addThemeLocalStorage, useDarkTheme, refsTheme } from './js/colorTheme';
+window.onload = useDarkTheme;
 
+addThemeLocalStorage();
 getLocalStorage();
 
+refsTheme.checkbox.addEventListener('click', useDarkTheme);
 // Modal listeners------------------------
 refs.movieContainer.addEventListener('click', renderModalFilmCard);
 refs.btnClose.addEventListener('click', closeModal);
