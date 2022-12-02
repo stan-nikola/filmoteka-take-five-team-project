edc746ee0105f9f007a87f57047c76c99decefb3
Alexander Schlieden <alex.schlieden@gmail.com>
2022-12-02 13:49:52 +0100

    disable Watch Trailer Btn

--- src/js/renderMovieTrailerModal.js
+++ src/js/renderMovieTrailerModal.js
@@ -10,6 +10,11 @@ export async function renderTrailerModal(event) {
       option.name === 'Official Trailer' ||
       option.name === 'Official Trailer [Subtitled]'
   );
+  const trailercontainer = document.querySelector(
+    '.modal-card__trailer__video__js'
+  );
+  const trailerBtn = document.querySelector('.btn__trailer__modal__js');
+  const trailerInfo = document.querySelector('.info__no__trailer__modal__js');
   if (trailerObject) {
     // console.log('TRAILER KEY ', trailerObject.key);
     console.log('TRAILER IFRAME IS CREATET');
@@ -18,13 +23,10 @@ export async function renderTrailerModal(event) {
         <iframe width=100 % " height="250" src='https://www.youtube.com/embed/${trailerObject.key}'frameborder = "0" allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe >
     </div>
       `;
-    const trailercontainer = document.querySelector(
-      '.modal-card__trailer__video__js'
-    );
     trailercontainer.insertAdjacentHTML('afterbegin', trailerMarkup);
+    trailerBtn.disabled = true;
     return;
   } else {
-    const trailerInfo = document.querySelector('.info__no__trailer__modal__js');
     trailerInfo.style.display = 'block';
   }
 }
