b9d0839a7f5a12670ccfefddb355194b1acb0a67
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2022-12-03 20:19:41 +0200

    comment logs fix markup

--- src/js/footerModal.js
+++ src/js/footerModal.js
@@ -1,155 +1,133 @@
 import { team } from '../data/team.js';
 
 import staskarmanov from '../images/team/stanislav-karmanov.jpeg';
-console.log( staskarmanov );
+// console.log(staskarmanov);
 
 import dariapluto from '../images/team/daria-pluto.png';
-console.log( dariapluto );
+// console.log(dariapluto);
 
 import maksimshvets from '../images/team/maksim-shviets.jpeg';
-console.log( maksimshvets )
+// console.log(maksimshvets);
 
-import sergeyj from '../images/team/sergey-j.png'
-console.log( sergeyj );
+import sergeyj from '../images/team/sergey-j.png';
+// console.log(sergeyj);
 
 import anton from '../images/team/anton-provorov.jpg';
-console.log( anton );
+// console.log(anton);
 
-import ira from '../images/team/ira-rybka.jpeg'
-console.log( ira );
+import ira from '../images/team/ira-rybka.jpeg';
+// console.log(ira);
 
 import valentyn from '../images/team/valentyn.jpeg';
-console.log( valentyn );
+// console.log(valentyn);
 
 import slava from '../images/team/slava.jpeg';
-console.log( slava );
+// console.log(slava);
 
 import oleh from '../images/team/oleh.png';
-console.log( oleh );
+// console.log(oleh);
 
-import alex from '../images/team/alex-schlieden.jpeg'
-console.log( alex );
+import alex from '../images/team/alex-schlieden.jpeg';
+// console.log(alex);
 
 import orest from '../images/team/orest.jpeg';
-console.log( orest );
+// console.log(orest);
 
 import natalie from '../images/team/natalie.png';
-console.log( natalie );
+// console.log(natalie);
 
-import iconNets from '../images/icons/icons-social-nets.svg'
-console.log( iconNets );
+import iconNets from '../images/icons/icons-social-nets.svg';
+// console.log(iconNets);
 
-
-console.log( team );
+// console.log(team);
 
 const footerRefs = {
-footerModalBtn: document.querySelector( '.js-modal' ),
-footerModalBackdrop: document.querySelector( ".footer__backdrop" ),
-teamList: document.querySelector( ".team__list" ),
-footerModalCloseBtn: document.querySelector( '.modal-close__btn' ),
-}
+  footerModalBtn: document.querySelector('.js-modal'),
+  footerModalBackdrop: document.querySelector('.footer__backdrop'),
+  teamList: document.querySelector('.team__list'),
+  footerModalCloseBtn: document.querySelector('.modal-close__btn'),
+};
 
+footerRefs.footerModalBtn.addEventListener('click', showModalHandler);
 
-footerRefs.footerModalBtn.addEventListener( "click", showModalHandler );
+function showModalHandler(event) {
+  footerRefs.footerModalBackdrop.classList.remove('hidden');
 
-
-function showModalHandler( event )
-{
-   
-    footerRefs.footerModalBackdrop.classList.remove( "hidden" );
-
-        
-    const teamMarkup = team.map( ( item ) => ` <li class="team__list-item">
-
-            <div class="teammate__card">
-                        <div class="img__thumb">
-                            <img src=${item.img} alt="" class="teammate__picture">
-
-                        </div>
-
-                        <div class="social__nets">
-
-                    <ul class="team__nets-list">
-
-                        <li class="team__nets-list-item">
-                            <a href=${item.git} class="team__net-icon-link">
-                                <div class="team__net-icon-thumb">
-                                    <svg class="team__net-icon" width="30px" height="30px">
-                                        <use href="http://localhost:1234/icons-social-nets.473c5594.svg?1670009233147#icon-git-opt"></use>
-                                    </svg>
-                                </div>
-                            </a>
-                        </li>
-
-
-                        <li class="team__nets-list-item">
-                            <a href=${item.linkedin} class="team__net-icon-link">
-                                <div class="team__net-icon-thumb">
-                                    <svg class="team__net-icon" width="30px" height="30px">
-                                        <use href="http://localhost:1234/icons-social-nets.473c5594.svg?1670009233147#icon-linkedin-opt"></use>
-                                    </svg>
-                                </div>
-                            </a>
-                        </li>
-
-
-
-                    </ul>
-
-
-                </div>
-
-
-
-
-
-                        <div class="teammate__info">
-                            <div class="info__thumb">
-                                <p class="name">${item.name}</p>
-                                <p class="role">${item.role}</p>
-                            </div>
-                            <p class="responsibilities">${item.resposibilities}</p>
-
-
-                        </div>
-
-                
-
-
+  const teamMarkup = team
+    .map(
+      item => `<li class="team__list-item">
+  <div class="teammate__card">
+    <div class="img__thumb">
+      <img src=${item.img} alt="" class="teammate__picture">
+    </div>
+    <div class="social__nets">
+      <ul class="team__nets-list">
+        <li class="team__nets-list-item">
+          <a href=${item.git} class="team__net-icon-link">
+            <div class="team__net-icon-thumb">
+              <svg class="team__net-icon" width="30px" height="30px">
+                <use href="http://localhost:1234/icons-social-nets.473c5594.svg?1670009233147#icon-git-opt"></use>
+              </svg>
             </div>
+          </a>
+        </li>
+        <li class="team__nets-list-item">
+          <a href=${item.linkedin} class="team__net-icon-link">
+            <div class="team__net-icon-thumb">
+              <svg class="team__net-icon" width="30px" height="30px">
+                <use href="http://localhost:1234/icons-social-nets.473c5594.svg?1670009233147#icon-linkedin-opt"></use>
+              </svg>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="teammate__info">
+      <div class="info__thumb">
+        <p class="name">${item.name}</p>
+        <p class="role">${item.role}</p>
+      </div>
+      <p class="responsibilities">${item.resposibilities}</p>
+    </div>
+  </div>
+</li> `
+    )
+    .join('');
 
-            </li>`).join( "" );
-    
-    console.log( teamMarkup );
-    
-    footerRefs.teamList.innerHTML = teamMarkup;
+  //   console.log(teamMarkup);
 
-    footerRefs.footerModalCloseBtn.addEventListener( 'click', closeFooterModalHandler );
-    window.addEventListener( 'keydown', onKeyCloseFooterModal );
-    footerRefs.footerModalBackdrop.addEventListener( 'click', onBackdropClickClose );
-  
+  footerRefs.teamList.innerHTML = teamMarkup;
+
+  footerRefs.footerModalCloseBtn.addEventListener(
+    'click',
+    closeFooterModalHandler
+  );
+  window.addEventListener('keydown', onKeyCloseFooterModal);
+  footerRefs.footerModalBackdrop.addEventListener(
+    'click',
+    onBackdropClickClose
+  );
 }
 
-function closeFooterModalHandler()
-{
-    footerRefs.footerModalBackdrop.classList.add('hidden');
-    window.removeEventListener('keydown', onKeyCloseFooterModal);
-    footerRefs.footerModalBackdrop.removeEventListener('click', onBackdropClickClose);
- }
-
-
+function closeFooterModalHandler() {
+  footerRefs.footerModalBackdrop.classList.add('hidden');
+  window.removeEventListener('keydown', onKeyCloseFooterModal);
+  footerRefs.footerModalBackdrop.removeEventListener(
+    'click',
+    onBackdropClickClose
+  );
+}
 
 function onKeyCloseFooterModal(evt) {
-  const ESC_KEY_CODE = 'Escape'
+  const ESC_KEY_CODE = 'Escape';
   evt.preventDefault();
   if (evt.code === ESC_KEY_CODE) {
-    closeFooterModalHandler()
+    closeFooterModalHandler();
   }
 }
 
-
-function onBackdropClickClose (ev) {
-  if(ev.currentTarget === ev.target) {
+function onBackdropClickClose(ev) {
+  if (ev.currentTarget === ev.target) {
     closeFooterModalHandler();
   }
 }
