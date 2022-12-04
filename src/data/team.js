7974041fa1742fed8634fd7748a784b9cbabc0b8
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2022-12-04 12:15:19 +0200

    fix footer modal

--- src/data/team.js
+++ src/data/team.js
@@ -2,7 +2,7 @@ export const team = [
   {
     name: 'Stanislav Karmanov',
     role: 'Team Lead',
-    resposibilities:
+    responsibilities:
       'Project architecture, common best practices, code review, mentoring, scope assignment, load spinner, notifications, etc.',
     img1x: '',
     img2x: '',
@@ -11,13 +11,12 @@ export const team = [
 
     linkedin: 'https://www.linkedin.com/in/stanislav-karmanov-b58197241/',
     git: 'https://github.com/stan-nikola',
-
   },
 
   {
     name: 'Дарья Панчук',
     role: 'Scrum master',
-    resposibilities:
+    responsibilities:
       'Регулювала термін виконання проєкту. Здійснювала щоденний Stand Up для планування роботи команди. Слідкувала за наявністю завдань у всіх учасників команди та термінами їх реалізації у Trello. Допомагала вдосконалювати стилі проєкту. Реалізувала реєстрацію за допомогою бези Firebase.',
     img1x: '',
     img2x: '',
@@ -30,46 +29,45 @@ export const team = [
   {
     name: 'Максим Швец',
     role: 'Front-end developer',
-    resposibilities: 'Логика кнопок MyLibrary',
+    responsibilities: 'Логика кнопок MyLibrary',
     img1x: '',
     img2x: '',
     img3x: '',
     img: 'https://stan-nikola.github.io/filmoteka-take-five-team-project/maksim-shviets.d9dc0fb1.jpeg',
 
-    linkedin: "",
-    git: "",
-
+    linkedin: '',
+    git: '',
   },
 
   {
     name: 'Сергей Алексеев',
     role: 'Front-end developer',
-    resposibilities:
+    responsibilities:
       'Поиск и отображение фильмов по запросу, обработка некорректного ввода и ошибок вывода; верстка и стилизация галереи и карточки фильма.',
     img1x: '',
     img2x: '',
     img3x: '',
     img: 'https://stan-nikola.github.io/filmoteka-take-five-team-project/sergey-j.1f3b07f0.png',
-    linkedin: "",
-    git: "https://github.com/Alex-S17/",
+    linkedin: '',
+    git: 'https://github.com/Alex-S17/',
   },
 
   {
     name: 'Антон Проворов',
     role: 'Front-end developer',
-    resposibilities: 'Верстка хедера страниц, переключение между страницами',
+    responsibilities: 'Верстка хедера страниц, переключение между страницами',
     img1x: '',
     img2x: '',
     img3x: '',
     img: 'https://stan-nikola.github.io/filmoteka-take-five-team-project/anton-provorov.08e493dc.jpg',
-    linkedin: "",
-    git: "",
+    linkedin: '',
+    git: '',
   },
 
   {
     name: 'Ирина Рыбка',
     role: 'Front-end developer',
-    resposibilities:
+    responsibilities:
       'Верстка модального окна карточки фильма, реализация отрытия и закрытия модального окна',
     img1x: '',
     img2x: '',
@@ -77,87 +75,82 @@ export const team = [
     img: 'https://stan-nikola.github.io/filmoteka-take-five-team-project/ira-rybka.bf6773cc.jpeg',
     linkedin: '',
     git: '',
-
   },
 
   {
     name: 'Валентин Мусиенко',
     role: 'Front-end developer',
-    resposibilities: 'Верстка и логика пагинации',
+    responsibilities: 'Верстка и логика пагинации',
     img1x: '',
     img2x: '',
     img3x: '',
     img: 'https://stan-nikola.github.io/filmoteka-take-five-team-project/valentyn.2327cc9d.jpeg',
 
-    linkedin: "",
-    git: "",
-
+    linkedin: '',
+    git: '',
   },
 
   {
     name: 'Вячеслав Герасюта',
     role: 'Front-end developer',
-    resposibilities:
+    responsibilities:
       'Рендеринг определенной части фильмов при переходе по страницам пагинации',
     img1x: '',
     img2x: '',
     img3x: '',
     img: 'https://stan-nikola.github.io/filmoteka-take-five-team-project/slava.15fbdc02.jpeg',
 
-    linkedin: "",
-    git: "",
+    linkedin: '',
+    git: '',
   },
 
   {
     name: 'Олег Галицкий',
     role: 'Front-end developer',
-    resposibilities: 'Верстка и подключение модалки с трейлером фильма',
+    responsibilities: 'Верстка и подключение модалки с трейлером фильма',
     img1x: '',
     img2x: '',
     img3x: '',
     img: 'https://stan-nikola.github.io/filmoteka-take-five-team-project/oleh.cf6c7e04.png',
-    linkedin: "",
-    git: "",
-
+    linkedin: '',
+    git: '',
   },
 
   {
     name: 'Алекс Шлиден',
     role: 'Front-end developer',
-    resposibilities:
+    responsibilities:
       'Верстка хедера страницы My Library, реализация подгрузки фильма на главную страницу',
     img1x: '',
     img2x: '',
     img3x: '',
     img: 'https://stan-nikola.github.io/filmoteka-take-five-team-project/alex-schlieden.4e5af8cb.jpeg',
-    linkedin: "",
-    git: "",
+    linkedin: '',
+    git: '',
   },
 
   {
     name: 'Орест Мазгаль',
     role: 'Front-end developer',
-    resposibilities: 'Реализация слайдера с превью фильмов',
+    responsibilities: 'Реализация слайдера с превью фильмов',
     img1x: '',
     img2x: '',
     img3x: '',
     img: 'https://stan-nikola.github.io/filmoteka-take-five-team-project/orest.9ef72526.jpeg',
-    linkedin: "",
-    git: "",
-
+    linkedin: '',
+    git: '',
   },
 
   {
     name: 'Ткаченко Наталья',
     role: 'Front-end developer',
-    resposibilities:
+    responsibilities:
       'Верстка футера, верстка и подключение модального окна футера',
     img1x: '',
     img2x: '',
     img3x: '',
     img: 'https://stan-nikola.github.io/filmoteka-take-five-team-project/natalie.a36f784c.png',
-    linkedin: "https://www.linkedin.com/in/natalie-tkachenko-/",
-    git: "https://github.com/NatalieTkachenko",
-
+    linkedin: 'https://www.linkedin.com/in/natalie-tkachenko-/',
+    git: 'https://github.com/NatalieTkachenko',
   },
 ];
--- src/js/footerModal.js
+++ src/js/footerModal.js
@@ -1,151 +1,104 @@
 import { team } from '../data/team.js';
-
-import staskarmanov from '../images/team/stanislav-karmanov.jpeg';
-// console.log(staskarmanov);
-
-import dariapluto from '../images/team/daria-pluto.png';
-// console.log(dariapluto);
-
-import maksimshvets from '../images/team/maksim-shviets.jpeg';
-// console.log(maksimshvets);
-
-import sergeyj from '../images/team/sergey-j.png';
-// console.log(sergeyj);
-
-import anton from '../images/team/anton-provorov.jpg';
-// console.log(anton);
-
-import ira from '../images/team/ira-rybka.jpeg';
-// console.log(ira);
-
-import valentyn from '../images/team/valentyn.jpeg';
-// console.log(valentyn);
-
-import slava from '../images/team/slava.jpeg';
-// console.log(slava);
-
-import oleh from '../images/team/oleh.png';
-// console.log(oleh);
-
-import alex from '../images/team/alex-schlieden.jpeg';
-// console.log(alex);
-
-import orest from '../images/team/orest.jpeg';
-// console.log(orest);
-
-import natalie from '../images/team/natalie.png';
-// console.log(natalie);
-
+// import staskarmanov from '../images/team/stanislav-karmanov.jpeg';
+// import dariapluto from '../images/team/daria-pluto.png';
+// import maksimshvets from '../images/team/maksim-shviets.jpeg';
+// import sergeyj from '../images/team/sergey-j.png';
+// import anton from '../images/team/anton-provorov.jpg';
+// import ira from '../images/team/ira-rybka.jpeg';
+// import valentyn from '../images/team/valentyn.jpeg';
+// import slava from '../images/team/slava.jpeg';
+// import oleh from '../images/team/oleh.png';
+// import alex from '../images/team/alex-schlieden.jpeg';
+// import orest from '../images/team/orest.jpeg';
+// import natalie from '../images/team/natalie.png';
 import iconNets from '../images/icons/icons-social-nets.svg';
-// console.log(iconNets);
-
-// console.log(team);
 
 const footerRefs = {
-body: document.querySelector("body"),
-footerModalBtn: document.querySelector( '.js-modal' ),
-footerModalBackdrop: document.querySelector( ".footer__backdrop" ),
-teamList: document.querySelector( ".team__list" ),
-footerModalCloseBtn: document.querySelector( '.modal-close__btn' ),
+  body: document.querySelector('body'),
+  footerModalBtn: document.querySelector('.js-modal'),
+  footerModalBackdrop: document.querySelector('.footer__backdrop'),
+  teamList: document.querySelector('.team__list'),
+  footerModalCloseBtn: document.querySelector('.modal-close__btn'),
+};
+
+console.log(footerRefs.body);
+
+footerRefs.footerModalBtn.addEventListener('click', showModalHandler);
+
+function showModalHandler(event) {
+  footerRefs.footerModalBackdrop.classList.remove('hidden');
+
+  const teamMarkup = team
+    .map(
+      item => `
+<li class="team__list-item">
+  <div class="teammate__card">
+    <div class="icons-position">
+      <div class="img__thumb">
+        <img src=${item.img} alt="" class="teammate__picture">
+      </div>
+      <div class="social__nets">
+        <ul class="team__nets-list">
+          <li class="team__nets-list-item">
+            <a href=${item.git} class="team__net-icon-link">
+              <div class="team__net-icon-thumb">
+                <svg class="team__net-icon" width="30px" height="30px">
+                  <use href="https://stan-nikola.github.io/filmoteka-take-five-team-project/icons-social-nets.473c5594.svg?1670009233147#icon-git-opt"></use>
+                </svg>
+              </div>
+            </a>
+          </li>
+          <li class="team__nets-list-item">
+            <a href=${item.linkedin} class="team__net-icon-link">
+              <div class="team__net-icon-thumb">
+                <svg class="team__net-icon" width="30px" height="30px">
+                  <use href="https://stan-nikola.github.io/filmoteka-take-five-team-project/icons-social-nets.473c5594.svg?1670009233147#icon-linkedin-opt">
+                  </use>
+                </svg>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="teammate__info">
+      <div class="info__thumb">
+        <p class="name">${item.name}</p>
+        <p class="role">${item.role}</p>
+      </div>
+      <p class="responsibilities">${item.responsibilities}</p>
+    </div>
+  </div>
+</li>`
+    )
+    .join('');
+
+  console.log(teamMarkup);
+
+  footerRefs.teamList.innerHTML = teamMarkup;
+
+  footerRefs.body.style.overflow = 'hidden';
+
+  footerRefs.footerModalCloseBtn.addEventListener(
+    'click',
+    closeFooterModalHandler
+  );
+  window.addEventListener('keydown', onKeyCloseFooterModal);
+  footerRefs.footerModalBackdrop.addEventListener(
+    'click',
+    onBackdropClickClose
+  );
 }
 
-console.log( footerRefs.body );
-
-footerRefs.footerModalBtn.addEventListener( "click", showModalHandler );
-
-
-function showModalHandler( event )
-{
-   
-    footerRefs.footerModalBackdrop.classList.remove( "hidden" );
-
-        
-    const teamMarkup = team.map( ( item ) => ` <li class="team__list-item">
-
-            <div class="teammate__card">
-
-
-            <div class="icons-position">
-                        <div class="img__thumb">
-                            <img src=${item.img} alt="" class="teammate__picture">
-
-                        </div>
-
-                        <div class="social__nets">
-
-                    <ul class="team__nets-list">
-
-                        <li class="team__nets-list-item">
-                            <a href=${item.git} class="team__net-icon-link">
-                                <div class="team__net-icon-thumb">
-                                    <svg class="team__net-icon" width="30px" height="30px">
-                                        <use href="http://localhost:1234/icons-social-nets.473c5594.svg?1670009233147#icon-git-opt"></use>
-                                    </svg>
-                                </div>
-                            </a>
-                        </li>
-
-
-                        <li class="team__nets-list-item">
-                            <a href=${item.linkedin} class="team__net-icon-link">
-                                <div class="team__net-icon-thumb">
-                                    <svg class="team__net-icon" width="30px" height="30px">
-                                        <use href="http://localhost:1234/icons-social-nets.473c5594.svg?1670009233147#icon-linkedin-opt"></use>
-                                    </svg>
-                                </div>
-                            </a>
-                        </li>
-
-
-
-                    </ul>
-
-
-                </div>
-
-            </div>
-
-
-
-                        <div class="teammate__info">
-                            <div class="info__thumb">
-                                <p class="name">${item.name}</p>
-                                <p class="role">${item.role}</p>
-                            </div>
-                            <p class="responsibilities">${item.resposibilities}</p>
-
-
-                        </div>
-
-                
-
-
-            </div>
-
-            </li>`).join( "" );
-    
-    console.log( teamMarkup );
-    
-    footerRefs.teamList.innerHTML = teamMarkup;
-
-    footerRefs.body.style.overflow = "hidden";
-
-    footerRefs.footerModalCloseBtn.addEventListener( 'click', closeFooterModalHandler );
-    window.addEventListener( 'keydown', onKeyCloseFooterModal );
-    footerRefs.footerModalBackdrop.addEventListener( 'click', onBackdropClickClose );
-  
+function closeFooterModalHandler() {
+  footerRefs.footerModalBackdrop.classList.add('hidden');
+  footerRefs.body.style.overflow = 'auto';
+  window.removeEventListener('keydown', onKeyCloseFooterModal);
+  footerRefs.footerModalBackdrop.removeEventListener(
+    'click',
+    onBackdropClickClose
+  );
 }
-
-function closeFooterModalHandler()
-{
-    footerRefs.footerModalBackdrop.classList.add('hidden');
-    footerRefs.body.style.overflow = "auto";
-    window.removeEventListener( 'keydown', onKeyCloseFooterModal );
-    footerRefs.footerModalBackdrop.removeEventListener('click', onBackdropClickClose);
- }
-
-
-
 
 function onKeyCloseFooterModal(evt) {
   const ESC_KEY_CODE = 'Escape';
