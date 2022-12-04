b2fd7a5333a8e9abae1b237a622d59ffb233f8ed
Alexander Schlieden <alex.schlieden@gmail.com>
2022-12-04 09:00:17 +0100

    bug fix modal trailer

--- src/js/renderMovieTrailerModal.js
+++ src/js/renderMovieTrailerModal.js
@@ -1,11 +1,10 @@
 import { fetchTrailer } from './apiService';
-
 import { notificationWarning } from './notifications';
-
+let getElem = selector => document.querySelector(selector);
 export async function renderTrailerModal(event) {
   let trailerId = event.target.dataset.movieid;
 
-  const trailerBtn = document.querySelector('.btn__trailer__modal__js');
+  // const trailerBtn = document.querySelector('.btn__trailer__modal__js');
 
   const dataTrailer = await fetchTrailer(trailerId);
   const trailerObject = dataTrailer.results.find(
@@ -13,18 +12,17 @@ export async function renderTrailerModal(event) {
       option.name === 'Official Trailer' ||
       option.name === 'Official Trailer [Subtitled]'
   );
-  const trailercontainer = document.querySelector(
+  const trailerContainer = document.querySelector(
     '.modal-card__trailer__video__js'
   );
   if (trailerObject) {
-    console.log('TRAILER IFRAME IS CREATET');
     const trailerMarkup = `
     <div>
         <iframe width=100 % " height="250" src='https://www.youtube.com/embed/${trailerObject.key}'frameborder = "0" allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe >
     </div>
       `;
-    trailercontainer.insertAdjacentHTML('afterbegin', trailerMarkup);
-    trailerBtn.disabled = true;
+    trailerContainer.insertAdjacentHTML('afterbegin', trailerMarkup);
+    getElem('.btn__trailer__modal__js').disabled = true;
     return;
   } else {
     notificationWarning('Sorry, trailer not available');
